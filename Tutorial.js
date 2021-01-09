727e70178858fc88c3604afadcab61a5a42d7950
HamzaKarim1998 <hamzakarim1998@gmail.com>
2021-01-08 19:28:55 -0500

    Added and commited Tutoruial, index, module1 .js files

---
+++ Tutorial.js
@@ -0,0 +1,5 @@
+const sum = require("./module1");
+
+const module1 = require("./module1.js");
+console.log(module1);
+//sum(2,2)
---
+++ module1.js
@@ -0,0 +1,8 @@
+const sum = (num1, num2) => num1 + num2;
+const Pi = 3.14;
+class objectExample {
+  constructor() {
+    console.log("object of class objectExample being created");
+  }
+}
+module.exports = { sum: sum, Pi: Pi, objectExample: objectExample };
